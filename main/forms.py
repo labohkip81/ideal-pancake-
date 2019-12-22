bc6983fd1d516f80783901c93a0938366c9d850c
gg <labankiplagat81@gmail.com>
2019-12-22 14:50:45 +0300

    Rectified errors on failing tests in django.

--- main/forms.py
+++ main/forms.py
@@ -3,11 +3,11 @@ from django.core.mail import send_mail
 import logging
 
 
-logging = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
-    message = forms.CharField(max_length=600, widget=forms.Textarea())'
+    message = forms.CharField(max_length=600, widget=forms.Textarea())
 
     def send_mail(self):
         logger.info(" Sending email to customer service ")
