6ddf5c651cd26666809d50b38d1682d1575ddc9a
gg <labankiplagat81@gmail.com>
2019-12-23 10:17:12 +0300

    successfully added custom forms to the app.

--- main/forms.py
+++ main/forms.py
@@ -8,12 +8,18 @@ logger = logging.getLogger(__name__)
 class ContactForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
     message = forms.CharField(max_length=600, widget=forms.Textarea())
+    comment = forms.CharField(max_length=500, widget=forms.Textarea())
 
     def send_mail(self):
         logger.info(" Sending email to customer service ")
-        message = " From: {0}\n{1}".format(
+        message = " From: {0}\n{1}\n{2}".format(
             self.cleaned_data["name"],
             self.cleaned_data["message"],
+            self.cleaned_data["comment"],
+
+
+
+
         )
 
         send_mail(
