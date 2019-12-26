7cc51c080bf9591039dc61f177a7eca66c6d8d38
gg <labankiplagat81@gmail.com>
2019-12-26 20:30:58 +0300

    created product tag and product image models for the different items represented on the app.

--- main/models.py
+++ main/models.py
@@ -11,3 +11,15 @@ class Product(models.Model):
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE )
+    
+    #the below model needs pillow so that it can function well.
+    image = models.ImageField(upload_to="product-images")
+
+class ProductTag(models.Model):
+    products = models.ManyToManyField(Product, blank=True)
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=32)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
