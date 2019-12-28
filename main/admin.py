d80c1a3302784cfb817c4b10632aafff656fb6f2
gg <labankiplagat81@gmail.com>
2019-12-28 11:17:00 +0300

    model admi for product page

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from . import models
 
 # Register your models here.
 
-admin.site.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'in_stock', 'price')
+    list_filter = ('active', 'in_stock', 'date_updated', 'price')
+    list_editable = ('in_stock',)
+    search_fields = ('name',)
+    prepopulated_fields = {"slug":("name",)}
+    #readonly_fields = ('slug',)
+
+admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.ProductTag)
 admin.site.register(models.ProductImage)
