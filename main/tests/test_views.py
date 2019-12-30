ba4e3899be751197b26f41b098d705532685d9a1
gg <labankiplagat81@gmail.com>
2019-12-30 05:32:33 +0300

    designed the product list page.

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
+from decimal import Decimal
+
+from main import models
+
 # Create your tests here.
 class TestPage(TestCase):
     def test_home_page_works(self):
--- main/views.py
+++ main/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 from django.views.generic.edit import FormView
+
+from main import models
 from main import forms 
+
+
+class ProductListView(ListView):
+    template_name = "main/product_list.html"
+    paginate_by = 4
+
+    def get_queryset(self):
+        tag = self.kwargs['tags']
+        self.tag = None
+        if tag != "all":
+            self.tag = get_object_or_404(model.ProductTag, slug=tag)
+        
+        if self.tag:
+            product = models.Product.objects.active().filter(tags=self.tag)
+        
+        else:
+            products = models.Product.objects.active()
+
+        return products.order_by("name")
+        
+
 
 class ContactUsView(FormView):
     template_name = "contact_form.html"
