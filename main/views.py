e961c8befcf5a15d86d1c0ecb262156e4292a815
gg <labankiplagat81@gmail.com>
2019-12-31 10:07:36 +0300

    stopped on the addressupdateview.

--- main/views.py
+++ main/views.py
@@ -4,7 +4,9 @@ from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
-
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import  LoginRequiredMixin
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 from django.views.generic.edit import FormView
@@ -69,3 +71,35 @@ class ContactUsView(FormView):
     def form_valid(self, form):
         form.send_mail()
         return super().form_valid(form)
+
+
+#adress lists to support CRUD operations in django.
+
+class AddressListView(LoginRequiredMixin, ListView):
+    model = models.Address
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+class AddressCreateView(LoginRequiredMixin, CreateView):
+    model = models.Address
+
+    fields = [
+        "name",
+        "address1",
+        "address2",
+        "zip_code",
+        "city",
+        "country",
+    ]
+
+    success_url = reverse_lazy("address_list")
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.user = self.request.user
+        obj.save()
+        return super().form_valid(form)
+
+class AddressUpdateView(LoginRequiredMixin, UpdateView):
+    
