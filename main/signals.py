b0fe9f0292ee93460e785abb162f6e6767b50264
gg <labankiplagat81@gmail.com>
2020-01-03 22:50:08 +0300

    added widgets file to the app.

--- main/signals.py
+++ main/signals.py
@@ -4,6 +4,10 @@ from PIL import Image
 from django.core.files.base import ContentFile
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in
+
+
+
 from .models import ProductImage
 
 
@@ -30,3 +34,26 @@ def generate_thumbnail(sender, instance, **kwargs):
         save=False,
     )
     temp_thumb.close()
+
+@receiver(user_logged_in):
+def merge_baskets_if_found(sender, user, request, **kwargs):
+    anonymous_basket = getattr(request, "basket", None)
+    if anonymous_basket:
+        try:
+            loggedin_basket = Basket.objects.get(
+                user=user, status=Basket.OPEN
+            )
+            for line in anonymous_basket.basketline_set.all():
+                line.basket = loggedin_basket
+                line.save()
+            anonymous_basket.delete()
+            request.basket = loggedin_basket
+            logger.info(
+                "Merged basket to id %d", loggdin_basket.id
+            )
+    except Basket.DoesNotExist:
+        anonymous_basket.user = user
+        anonymous_basket.save()
+        logger.info("Assigned user to basket id %d",anonymous_basket.id,)
+
+
